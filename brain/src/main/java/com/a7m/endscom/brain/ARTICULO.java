1085604305ff413c6760a754cedb4dcbca7a2125
Ing.Maryan Espinoza <endscom@gmail.com>
2016-09-04 21:39:07 -0600

    04/09/2016

---
+++ brain/src/main/java/com/a7m/endscom/brain/ARTICULO.java
@@ -0,0 +1,96 @@
+package com.a7m.endscom.brain;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by A7M on 04/09/2016.
+ */
+public class ARTICULO {
+    private String Nombre,Precio,Descripcion;
+    private int IdArticulo;
+
+
+    public ARTICULO(String nombre, String precio, String descripcion, int idArticulo) {
+        Nombre = nombre;
+        Precio = precio;
+        Descripcion = descripcion;
+        IdArticulo = idArticulo;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+    public ARTICULO() {
+        super();
+    }
+
+    public int getIdArticulo() {
+        return IdArticulo;
+    }
+
+    public void setIdArticulo(int idArticulo) {
+        IdArticulo = idArticulo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(String precio) {
+        Precio = precio;
+    }
+    public static List<ARTICULO> getArticulos(String basedir, Context context) {
+        List<ARTICULO> lista = new ArrayList<>();
+        SQLiteDatabase myDataBase = null;
+        SQLiteHelper myDbHelper = null;
+
+        try {
+            myDbHelper = new SQLiteHelper(basedir, context);
+            myDataBase = myDbHelper.getReadableDatabase();
+            Cursor cursor = myDataBase.query(true, "CATALOGO", null, null, null, null, null, null, null);
+            //Cursor cursor = myDataBase.rawQuery("SELECT * FROM DEPARTAMENTOS", null);
+
+            if(cursor.getCount() > 0) {
+
+                cursor.moveToFirst();
+                while(!cursor.isAfterLast()) {
+                    ARTICULO tmp = new ARTICULO();
+                    tmp.setIdArticulo(cursor.getInt(cursor.getColumnIndex("IdArticulo")));
+                    tmp.setNombre(cursor.getString(cursor.getColumnIndex("Nombre")));
+                    tmp.setPrecio(cursor.getString(cursor.getColumnIndex("Precio")));
+                    tmp.setDescripcion(cursor.getString(cursor.getColumnIndex("Descripcion")));
+                    lista.add(tmp);
+                    cursor.moveToNext();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(myDataBase != null) {
+                myDataBase.close();
+            }
+            if(myDbHelper != null) {
+                myDbHelper.close();
+            }
+        }
+        return lista;
+    }
+}
---
+++ brain/src/main/java/com/a7m/endscom/brain/Clientes.java
@@ -0,0 +1,102 @@
+package com.a7m.endscom.brain;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by A7M on 04/09/2016.
+ */
+public class Clientes {
+    private String Nombre,Direccion,Cedula,Telefono;
+    private int IdDepartamento;
+
+
+    public Clientes(String nombre, String direccion, String cedula, String telefono, int idDepartamento) {
+        Nombre = nombre;
+        Direccion = direccion;
+        Cedula = cedula;
+        Telefono = telefono;
+        IdDepartamento = idDepartamento;
+    }
+    public Clientes() {
+        super();
+    }
+    public int getIdDepartamento() {
+        return IdDepartamento;
+    }
+
+    public void setIdDepartamento(int idDepartamento) {
+        IdDepartamento = idDepartamento;
+    }
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public String getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(String cedula) {
+        Cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+    public static List<Clientes> getDepartamentos(String basedir, Context context) {
+        List<Clientes> lista = new ArrayList<>();
+        SQLiteDatabase myDataBase = null;
+        SQLiteHelper myDbHelper = null;
+
+        try {
+            myDbHelper = new SQLiteHelper(basedir, context);
+            myDataBase = myDbHelper.getReadableDatabase();
+            Cursor cursor = myDataBase.query(true, "DEPARTAMENTOS", null, null, null, null, null, null, null);
+            //Cursor cursor = myDataBase.rawQuery("SELECT * FROM DEPARTAMENTOS", null);
+
+            if(cursor.getCount() > 0) {
+
+                cursor.moveToFirst();
+                while(!cursor.isAfterLast()) {
+                    Clientes tmp = new Clientes();
+                    tmp.setIdDepartamento(cursor.getInt(cursor.getColumnIndex("IdDepartamento")));
+                    tmp.setNombre(cursor.getString(cursor.getColumnIndex("Nombre")));
+                    lista.add(tmp);
+                    cursor.moveToNext();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(myDataBase != null) {
+                myDataBase.close();
+            }
+            if(myDbHelper != null) {
+                myDbHelper.close();
+            }
+        }
+        return lista;
+    }
+
+}
